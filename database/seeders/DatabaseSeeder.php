b3ae70d260a9c34a2b71bd6d5eaea0567d919bc5
mehdi <mehdialibakhshi03@gmail.com>
2024-11-05 15:46:21 +0330

    testttللل

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,19 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
- 
+
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
             'name' => 'Test User',
 
             'email' => 'test@example.com',
