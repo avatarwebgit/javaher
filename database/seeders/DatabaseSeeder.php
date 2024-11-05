9631c805e882ccbdd3db944a7235f3c3e404003e
mehdi <mehdialibakhshi03@gmail.com>
2024-11-05 16:01:19 +0330

    testttللل

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'name' => 'Test User',
+            'name' => 'Test User',
 
 
 
